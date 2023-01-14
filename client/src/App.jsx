26283db7c8cfa6dbdd00ce9583b84671245ffca9
Pengpa0012 <peng.ilikha@gmail.com>
2023-01-14 19:37:31 +0800

    fixed socket id

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,20 @@ function App() {
   const [messages, setMessages] = useState([
     {
       message: "",
-      self: "",
       id: "",
     }
   ])
+  const [socketId, setSocketId] = useState(null)
   const [room, setRoom] = useState()
-  const socket = io("ws://localhost:3000")
+  const [socket] = useState(io("ws://localhost:3000"))
 
   useEffect(() => {
-    socket.on("message", (arg) => {
-      console.log("ARR",arg)
-    })
+    setSocketId(socket.id);
     socket.on("new message", (payload) => {
-      setMessages([...messages, {message: payload.message, self: payload.id == socket.id ? true : false, id: payload.id}])
+      setMessages([...messages, {message: payload.message, id: payload.id}])
     })
-  }, [messages])
+
+  }, [messages]);
 
   const onSendMessage = (message) => {
     if(message.key !== "Enter" || message.target.value == "") return
@@ -43,7 +42,7 @@ function App() {
         <div className="p-3 h-80 border border-gray-500 rounded-md text-left message-box overflow-auto">
           {
             messages.length > 1 ? messages.filter(message => message.id != "").map((message, i) => ( 
-              <h1 key={`message-${i}`} className="bg-gray-700 p-2 mb-1 rounded-md">{`${message.self ? "You" : message.id}: ${message.message}`}</h1>
+              <h1 key={`message-${i}`} className="bg-gray-700 p-2 mb-1 rounded-md">{`${socketId == message.id ? "You" : `User ${message.id}`}: ${message.message}`}</h1>
             )) : undefined
           }
         </div>
